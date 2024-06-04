c3c9782995f270d3cb0c2588cc52f1c17d743c4f
yutadd <spyk4m49@gmail.com>
2024-06-04 16:12:09 +0900

    [fix] wrong model

--- opencv_streamlit.py
+++ opencv_streamlit.py
@@ -23,24 +23,27 @@ if uploaded_file is not None:
     # 画像をOpenCV形式に変換
     img_array = np.array(image)
     st.write("画像の形状:", img_array.shape)
-    model = YOLO('data//yolov8n.pt')  # 事前に学習したモデルのパスを指定
+    model = YOLO('data//best.pt')  # 事前に学習したモデルのパスを指定
     results = model(img_array)
     # 切り取った名刺を表示
     image_count=0
     for result in results:
-            boxes = result.boxes.xyxy.cpu().numpy()
-            for box in boxes:
-                if result.names[int(result.boxes.cls[0])] == 'meishi':
-                    x1, y1, x2, y2 = map(int, box)
-                    business_card = img_array[y1:y2, x1:x2]
-                    # 切り取った名刺を表示
-                    st.image(business_card, caption='切り取られた名刺。',width=150, use_column_width=True)
-                    output_path = "./tmp/output"+str(image_count)+".png"
-                    cv2.imwrite(output_path, business_card)
-                    st.write(f"切り取られた名刺が {output_path} に保存されました。")
-                    image_count+=1
-                else:
-                    print("this is not meishi")
+        boxes = result.boxes.xyxy.cpu().numpy()
+        confidences = result.boxes.conf.cpu().numpy()
+        labels = result.boxes.cls.cpu().numpy()
+        names=result.names
+        for box, confidence, label in zip(boxes, confidences, labels):
+            if confidence >= 0.8 and names[label] == 'meishi':
+                x1, y1, x2, y2 = map(int, box)
+                business_card = img_array[y1:y2, x1:x2]
+                # 切り取った名刺を表示
+                st.image(business_card, caption='切り取られた名刺。', width=150, use_column_width=True)
+                output_path = "./tmp/output" + str(image_count) + ".png"
+                cv2.imwrite(output_path, business_card)
+                st.write(f"切り取られた名刺が {output_path} に保存されました。")
+                image_count += 1
+            else:
+                print("{0},{1}".format(confidence,names[label]))
     answer_placeholder = st.empty()  # プレースホルダーを作成
     print("detected "+str(image_count)+" cards")
     client = OpenAI()
